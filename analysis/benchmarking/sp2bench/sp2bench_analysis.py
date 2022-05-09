a1a80f87855046f9198f8d24363333b614cfb62a
nandikajain <jainnandika@gmail.com>
2022-05-10 05:12:19 +0530

    benchmarking for different triples

--- analysis/benchmarking/sp2bench/sp2bench_analysis.py
+++ analysis/benchmarking/sp2bench/sp2bench_analysis.py
@@ -1,24 +1,29 @@
 import rdflib
 import time
 
-g = rdflib.Graph()
-g.parse("sp2b.n3")
-print(len(g))
+iter_ = ["1", "2", "3a", "3b", "3c", "4", "5a","5b", "6","7", "8", "9", "10", "11", "12a", "12b", "12c"]
+def calculate_time(fileName):
+      g = rdflib.Graph()
+      g.parse(fileName)
+      print(fileName, len(g))
+      
+      before_time = time.time()
+      for i in range(0, 10):
+            for i in iter_:
+                  f = open("./sp2b/queries/q"+i+".sparql")   
+                  query= f.read()
+                  qres = g.query(query)
 
-# before_time = time.time()
-# iter_ = ["1", "2", "3a", "3b", "3c", "4", "5a","5b", "6","7", "8", "9", "10", "11", "12a", "12b", "12c"]
-
-# for i in range(0, 10):
-#   for i in iter_:
-#     f = open("./sp2b/queries/q"+i+".sparql")   
-#     query= f.read()
-#     qres = g.query(query)
-
-# clock_diff = time.time()-before_time
-# clock_diff /= 10
-# clock_diff /= len(iter_)
-# string_to_write = "Average time taken is (in seconds):"+ str(clock_diff)
-
-# with open("sp2bench_result.txt",'w',encoding = 'utf-8') as f:
-#   f.write(string_to_write)
-# print(string_to_write)
+      clock_diff = time.time()-before_time
+      clock_diff /= 10
+      clock_diff /= len(iter_)
+      string_to_write = "Average time taken is (in seconds) for "+fileName+" :"+ str(clock_diff)+"\n"
+      return string_to_write
+            
+string_to_write = ""
+file_names = ["sp2b_50k.n3", "sp2b_100k.n3"]
+for i in file_names:
+      string_to_write += calculate_time(i)
+with open("sp2bench_result.txt",'w',encoding = 'utf-8') as f:
+  f.write(string_to_write)
+print(string_to_write)
